e67b64cc12ae016884ba5d83113ee0f93fc11102
Alexis Cazin <alexis.cazin@ynov.com>
2023-10-25 15:23:47 +0200

    qui suis-je

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 import streamlit as st
 # from PIL import Image
-import pandas as pd 
+# import pandas as pd 
 # from bokeh.plotting import figure
 
-st.title('test page')
-
+st.title('Qui suis-je')
