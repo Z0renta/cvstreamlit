6f7f188fe4fc8463370036a274e588a81e02801f
Alexis Cazin <alexis.cazin@ynov.com>
2023-11-14 23:50:44 +0100

    test

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-# from pathlib import Path
+from pathlib import Path
 
-import streamlit as st
+# import streamlit as st
 # from PIL import Image
-# import pandas as pd 
+# import pandas as pd
 # from bokeh.plotting import figure
 
-st.title('Qui suis-je')
+
+
+
+
+
+
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "style" / "main.css"
+resume_file = current_dir / "assets" / "cv2023.pdf"
+profile_pic = current_dir / "assets" / "pp.png"
+
+
+PAGE_TITLE = "CV WEB Alexis Cazin"
+PAGE_ICON = ":wave:"
+NAME = "Alexis Cazin"
+DESCRIPTION = """
+Etudiant en 2eme année à Paris Ynov Campus.
+"""
+EMAIL = "alexis.czin@gmail.com"
+EMAIL2 = "alexis.cazin@ynov.com"
+SOCIAL_MEDIA = {
+    "LinkedIn": "https://www.linkedin.com/in/alexis-cazin-b19836260/",
+    "GitHub": "https://github.com/Z0renta",
+}
