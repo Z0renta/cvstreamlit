738fce0b5dae23fc59bfea77f511ce4504a3d807
Alexis Cazin <alexis.cazin@ynov.com>
2023-11-15 01:42:27 +0100

    fin

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ from bokeh.plotting import figure
 
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "style" / "main.css"
-resume_file = current_dir / "assets" / "cv2023.pdf"
-pp = current_dir / "assets" / "pp.png"
+css_file = "./style/main.css"
+CV = "./font/cv2023.pdf"
+pp = "./font/pp.png"
 
 
 PAGE_TITLE = "CV WEB Alexis Cazin"
 PAGE_ICON = ":wave:"
 NAME = "Alexis Cazin"
 DESCRIPTION = """
-Etudiant en 2eme année à Paris Ynov Campus.
+Etudiant en B2 année à Paris Ynov Campus.
 """
 EMAIL = "alexis.czin@gmail.com"
 EMAIL2 = "alexis.cazin@ynov.com"
@@ -35,7 +35,8 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
+with open(CV, "rb") as pdf_file:
+    print(CV)
     PDFbyte = pdf_file.read()
 pp = Image.open(pp)
 
@@ -47,9 +48,9 @@ with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
     st.download_button(
-        label=" 📄 Download Resume",
+        label=" 📄 Examiné le CV",
         data=PDFbyte,
-        file_name=resume_file.name,
+        file_name=CV.split("\\")[-1],
         mime="application/octet-stream",
     )
     st.write("📫", EMAIL)
@@ -74,26 +75,36 @@ st.write(
 
 st.write("#")
 st.subheader("Competénces en informatiques")
-st.write(
-    """
-Java | Php | Python (POO)| 
-Golang | Javascript |
-HTML | CSS | Git |
-SQL | La gestion de reseaux,
-(windows server, ubuntu, debian)
-
-"""
-)
-
-st.write("#")
-st.subheader("🌍 Langues")
-
 # Load the CSV data into a Pandas DataFrame
-data = pd.DataFrame({'Language': ['Anglais', 'Français'], 'Scores': [5.5, 8]})
+data = pd.DataFrame({'Competence': ['Java', 'PHP', 'Python', 'Golang', 'Javascript', 'Html', 'CSS', 'Git', 'SQL', 'Réseaux'], 'Maitrise': [4, 6, 8, 4, 7, 9, 9, 5, 7, 6]})
 
 # Create a Bokeh figure
-p = figure(y_range=data['Language'], height = 300, width=600, title="Language Scores")
-p.hbar(y=data['Language'], right=data['Scores'], height=0.9)
+p = figure(y_range=data['Competence'], height = 300, width=600, title="Competence Maitriser")
+p.hbar(y=data['Competence'], right=data['Maitrise'], height=0.9)
+
+# Show the Bokeh figure in Streamlit
+st.bokeh_chart(p)
+
+# st.write(
+#     """
+# Java | Php | Python (POO)| 
+# Golang | Javascript |
+# HTML | CSS | Git |
+# SQL | La gestion de reseaux,
+# (windows server, ubuntu, debian)
+
+# """
+# )
+
+st.write("#")
+st.subheader("🌍 Linguistique")
+
+# Load the CSV data into a Pandas DataFrame
+data = pd.DataFrame({'Language': ['Anglais', 'Français'], 'Maitrise': [5.5, 8]})
+
+# Create a Bokeh figure
+p = figure(y_range=data['Language'], height = 300, width=600, title="Language Maitriser")
+p.hbar(y=data['Language'], right=data['Maitrise'], height=0.9)
 
 # Show the Bokeh figure in Streamlit
 st.bokeh_chart(p)
@@ -101,43 +112,61 @@ st.bokeh_chart(p)
 # st.write("#")
 st.write("---")
 # st.write("#")
-
+st.write("#")
 st.subheader("Formations")
 
-st.write(" -  Baccalauréat 2021/2022 (Physique, Mathématiques et Informatique)")
-st.write(" -  B2 Informatique")
-st.write("Paris Ynov Campus, Nanterre - 09.2022 / 04.2027")
+# Utiliser st.markdown() avec HTML pour créer un bloc de formation stylisé
+st.markdown(
+    """
+    <div style="border: 1px solid #ddd; border-radius: 5px; padding: 10px; box-shadow: 1px 2px 5px #ccc; margin-top: 10px;">
+        <p>Baccalauréat 2021/2022 (Physique, Mathématiques et Informatique)</p>
+        <p>B2 Informatique</p>
+        <p>Paris Ynov Campus, Nanterre - 09.2022 / 04.2027</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # st.write("\n")
 st.write("---")
 # st.write("\n")
+st.write("#")
 
 st.subheader("Expérience étudiante")
 
+st.markdown(
+    """
+    <div style="border: 1px solid #ddd; border-radius: 5px; padding: 10px; box-shadow: 1px 2px 5px #ccc; margin-top: 10px;">
+        <p>Jeux réalisés:</p>
+        <p>Hangman/Hangman Web - Language: Golang/ HTML</p>
+        <p>Snake - Language: Python </p>
+        <p>Web développement: </p>
+        <p>La réalisation des projets tels que ASCII ART Web via le Hangman - Language: Golang</p>
+        <p>Le site Forum - Language: HTML/CSS/Golang/SQL</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-st.write("Jeux réaliser")
 
-st.write("- Hangman/Hangman Web")
-st.write("- - Language: Golang/ HTML ")
-st.write("- Snake")
-st.write("- - Language: Python")
-
-
-st.write("🏴 Web développement:")
-
-st.write("- ➖ La réalisation des projets tels que ASCII ART Web ")
-st.write("- - 🛠 Technologies: Golang")
-st.write("- ➖ Le site Forum")
-st.write("- - Language: HTML/CSS/Golang/SQL")
 
 # st.write("#")
 st.write("---")
 # st.write("#")
 
 st.subheader("Expériences professionelles")
-st.write("Ouvrier Polyvalent")
-st.write("- Les vins Richard 01/07/2022 au 01/09/2022 puis 01/07/2023 au 01/09/2023")
-st.write("- - Preparateurs de commandes sur Fenwick puis Controlleur au retours vide ")
+
+st.markdown(
+    """
+    <div style="border: 1px solid #ddd; border-radius: 5px; padding: 10px; box-shadow: 1px 2px 5px #ccc; margin-top: 10px;">
+        <p>Ouvrier Polyvalent</p>
+        <p>Les vins Richard 01/07/2022 au 01/09/2022 puis 01/07/2023 au 01/09/2023</p>
+        <p>Preparateurs de commandes sur Fenwick puis Controlleur au retours vide </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 
 # st.write("#")
 st.write("---")
